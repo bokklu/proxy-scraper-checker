e9bb241005e9e7c32322af06e630b96e1f4c4bad
Daniel <danzanzu@gmail.com>
2020-06-03 00:05:08 +0200

    Increasing the retry count in pldown

--- src/proxyscrape/proxyscrape_scraper.py
+++ src/proxyscrape/proxyscrape_scraper.py
@@ -11,7 +11,7 @@ class ProxyScrapeScraper:
 
     @staticmethod
     async def scrape(session, proxy_type):
-        for retry in range(3):
+        for retry in range(10):
             try:
                 async with async_timeout.timeout(7):
                     async with session.get(f'{provider_connections["proxyscrape"]}&proxytype={str.lower(proxy_type.name)}') as response:
