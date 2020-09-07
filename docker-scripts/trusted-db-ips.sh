87f3ac0961a26d4f88a466407faa052badbb934b
Daniel <danielbuckle@betssongroup.com>
2020-09-07 16:42:46 +0200

    Renamed POSTGRES_ENVIRONMENT

--- docker-scripts/trusted-db-ips.sh
+++ docker-scripts/trusted-db-ips.sh
@@ -1,5 +1,4 @@
-echo "$ENVIRONMENT"
-if [ "$ENVIRONMENT" = "Production" ]
+if [ "$POSTGRES_ENVIRONMENT" = "Production" ]
 then
 	sed -i d /var/lib/postgresql/data/pg_hba.conf
 	echo "host all  postgres  ::1/128	  		md5" >> /var/lib/postgresql/data/pg_hba.conf
