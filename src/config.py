ca1ffce9328407499115334f77a7215fee7417f9
Daniel <danzanzu@gmail.com>
2020-09-09 13:03:13 +0200

    moving geo_db config to base

--- src/config.py
+++ src/config.py
@@ -5,6 +5,10 @@ class Base:
         pldown='https://www.proxy-list.download',
         proxyscrape='https://api.proxyscrape.com/?request=displayproxies'
     )
+    geo_db = dict(
+        city_db='/app/src/data/GeoLite2-City.mmdb',
+        asn_db='/app/src/data/GeoLite2-ASN.mmdb'
+    )
     proxyscrape_pool_amount = 2000
     pldown_pool_amount = 2000
     max_retries = 3
@@ -21,10 +25,6 @@ class DevelopmentConfig(Base):
         database='proxydb',
         user='postgres',
         password='devpassword'
-    )
-    geo_db = dict(
-        city_db='/app/src/data/GeoLite2-City.mmdb',
-        asn_db='/app/src/data/GeoLite2-ASN.mmdb'
     )
     proxyscrape_pool_amount = 500
 
@@ -43,10 +43,6 @@ class ProductionConfig(Base):
         port='5432',
         database='proxydb',
         user='postgres'
-    )
-    geo_db = dict(
-        city_db='/app/src/data/GeoLite2-City.mmdb',
-        asn_db='/app/src/data/GeoLite2-ASN.mmdb'
     )
     proxyscrape_pool_amount = 250
     pldown_pool_amount = 250
