38165cbd509dcbc06a2dc584074637f91a16c858
Daniel <danzanzu@gmail.com>
2020-09-21 11:49:58 +0200

    md5

--- docker-compose-scripts/db-scripts/trusted-db-ips.sh
+++ docker-compose-scripts/db-scripts/trusted-db-ips.sh
@@ -2,6 +2,6 @@ if [ "$POSTGRES_ENVIRONMENT" = "Production" ]
 then
 	sed -i d /var/lib/postgresql/data/pg_hba.conf
 	echo "host all  postgres  ::1/128	  		md5" >> /var/lib/postgresql/data/pg_hba.conf
-	echo "host all  postgres  127.0.0.1/32		ms5" >> /var/lib/postgresql/data/pg_hba.conf
+	echo "host all  postgres  127.0.0.1/32		md5" >> /var/lib/postgresql/data/pg_hba.conf
 	# add trusted remote ip addresses you want to allow connections to the database
 fi
