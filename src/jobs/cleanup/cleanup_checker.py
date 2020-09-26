399a7c10b52adb5d6c401e28bf6ea8b97edd18ef
Daniel <danielbuckle@betssongroup.com>
2020-09-26 17:35:07 +0200

    tab

--- src/jobs/cleanup/cleanup_checker.py
+++ src/jobs/cleanup/cleanup_checker.py
@@ -21,7 +21,7 @@ class CleanupChecker:
 
         http_proxies, https_proxies, http_https_proxies, socks4_proxies, socks5_proxies, socks4_socks5_proxies = [], [], [], [], [], []
 
-	self._proxy_repo.get_access_type = True
+	    self._proxy_repo.get_access_type = True
 
         for proxy_record in cleanup_proxy_records:
             proxy_type = proxy_record[0][2]
